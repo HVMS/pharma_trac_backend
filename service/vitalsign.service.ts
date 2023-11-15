4a1bfc5dc6603929547918d2750e97e2a5de9b39
HVMS <viralsid2330@gmail.com>
2023-11-14 23:15:48 -0400

    files updated to just add the vital sign data to the collection

--- service/vitalsign.service.ts
+++ service/vitalsign.service.ts
@@ -31,7 +31,7 @@ class VitalSignSerivce {
 
             await client.close();
 
-            return { vitalSignData: result, data };
+            return { result, data };
 
         } catch (error) {
             console.log('error in createUser is : ' + error);
